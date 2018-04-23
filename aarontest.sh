7b5aa9fb7a355b6c096a7dd3fd96c6561359c350
Aaron Molitor <ammolitor@gmail.com>
2018-04-23 08:09:42 -0500

    pull up some parameters

--- aarontest.sh
+++ aarontest.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-MFACODE=${1}
+PROFILE=${1}
+SERIALNUMBER=${2}
+MFACODE=${3}
 
 echo "AWS_ACCESS_KEY_ID is:     ${AWS_ACCESS_KEY_ID}"
 echo "AWS_SECRET_ACCESS_KEY is: ${AWS_SECRET_ACCESS_KEY}"
@@ -12,7 +14,7 @@ unset AWS_SECRET_ACCESS_KEY
 unset AWS_SESSION_TOKEN
 
 echo "Setting AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN"
-CREDS=$(aws sts get-session-token --serial-number arn:aws:iam::953558963498:mfa/aarontest --token-code ${MFACODE} --profile aarontest | jq -r ' . | .Credentials | "\(.AccessKeyId) \(.SecretAccessKey) \(.SessionToken)"')
+CREDS=$(aws sts get-session-token --serial-number ${SERIALNUMBER} --token-code ${MFACODE} --profile ${PROFILE} | jq -r ' . | .Credentials | "\(.AccessKeyId) \(.SecretAccessKey) \(.SessionToken)"')
 export AWS_ACCESS_KEY_ID=$(echo ${CREDS} | awk '{print $1}')
 export AWS_SECRET_ACCESS_KEY=$(echo ${CREDS} | awk '{print $2}')
 export AWS_SESSION_TOKEN=$(echo ${CREDS} | awk '{print $3}')
