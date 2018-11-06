9725691b1f34963b070a689b1252b3fea81478f2
Aaron Molitor <ammolito@amazon.com>
2018-11-06 14:08:58 +0000

    escape dollar sign in clone command

--- clone-all-codecommit-repos.sh
+++ clone-all-codecommit-repos.sh
@@ -7,7 +7,7 @@ REPOSITORY_NAMES=( $(${AWS_CMD} --output text codecommit list-repositories --que
 
 for repository in ${REPOSITORY_NAMES[@]}; do
     repo_http_url=$(${AWS_CMD} --output text codecommit get-repository --repository-name ${repository} --query "repositoryMetadata.cloneUrlHttp")
-    git -c credential.helper="!${AWS_CMD} codecommit credential-helper $@" -c credential.UseHttpPath=true clone ${repo_http_url}
+    git -c credential.helper="!${AWS_CMD} codecommit credential-helper \$@" -c credential.UseHttpPath=true clone ${repo_http_url}
     pushd ${repository}
     git config --add credential.helper "!${AWS_CMD} codecommit credential-helper \$@"
     git config --add credential.UseHttpPath true
