9989d0ceecb43e7bce090261ba9370560ef02651
Aaron Molitor <ammolitor@gmail.com>
2018-11-06 08:03:06 -0600

    add codecommit clone script, update vpn-reset for high sierra and newer cisco client

---
+++ clone-all-codecommit-repos.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+AWS_PROFILE="${1:-default}"
+AWS_CMD="aws --profile ${AWS_PROFILE}"
+
+REPOSITORY_NAMES=( $(${AWS_CMD} --output text codecommit list-repositories --query "repositories[].repositoryName" --output text) )
+
+for repository in ${REPOSITORY_NAMES[@]}; do
+    repo_http_url=$(${AWS_CMD} --output text codecommit get-repository --repository-name ${repository} --query "repositoryMetadata.cloneUrlHttp")
+    git -c credential.helper="!${AWS_CMD} codecommit credential-helper $@" -c credential.UseHttpPath=true clone ${repo_http_url}
+    pushd ${repository}
+    git config --add credential.helper "!${AWS_CMD} codecommit credential-helper \$@"
+    git config --add credential.UseHttpPath true
+    popd
+done
--- vpn-reset.sh
+++ vpn-reset.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
+set -ex
 sudo ifconfig en0 down
-sudo pkill vpnagentd ciscod
+sudo ps waux | awk '/cisco/{print $2}' | xargs sudo kill -9
 sleep 3
 sudo ifconfig en0 up
